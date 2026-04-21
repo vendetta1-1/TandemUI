b18bdf72b93e4172ffa7d12c47105ce6571eefad
imZontik <elvin-nadirov-321@mail.ru>
2026-04-21 21:26:54 +0300

    feat(uikit/tokens): add color tokens

--- uikit/src/commonMain/kotlin/com/uikit/layout/TandemUiShrinkingHeaderLayout.kt
+++ uikit/src/commonMain/kotlin/com/uikit/layout/TandemUiShrinkingHeaderLayout.kt
@@ -1,4 +1,4 @@
-package com.uikit.uikit.layout
+package com.uikit.layout
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
---
+++ uikit/src/commonMain/kotlin/com/uikit/tokens/Color.kt
@@ -0,0 +1,34 @@
+package com.uikit.tokens
+
+import androidx.compose.ui.graphics.Color
+
+object ColorPalette {
+    val blue50 = Color(0xFFE3F2FD)
+    val blue100 = Color(0xFFBBDEFB)
+    val blue200 = Color(0xFF90CAF9)
+    val blue300 = Color(0xFF64B5F6)
+    val blue400 = Color(0xFF42A5F5)
+    val blue500 = Color(0xFF2196F3)
+    val blue600 = Color(0xFF1E88E5)
+    val blue700 = Color(0xFF1565C0)
+    val blue800 = Color(0xFF0D47A1)
+    val blue900 = Color(0xFF0A2472)
+
+    val neutral0 = Color(0xFFFFFFFF)
+    val neutral50 = Color(0xFFFAFAFA)
+    val neutral100 = Color(0xFFF5F5F5)
+    val neutral200 = Color(0xFFEEEEEE)
+    val neutral300 = Color(0xFFE0E0E0)
+    val neutral400 = Color(0xFFBDBDBD)
+    val neutral500 = Color(0xFF9E9E9E)
+    val neutral600 = Color(0xFF757575)
+    val neutral700 = Color(0xFF616161)
+    val neutral800 = Color(0xFF424242)
+    val neutral900 = Color(0xFF212121)
+    val neutral1000 = Color(0xFF000000)
+
+    val green500 = Color(0xFF4CAF50)
+    val red500 = Color(0xFFF44336)
+    val orange500 = Color(0xFFFF9800)
+    val yellow500 = Color(0xFFFFEB3B)
+}
